97f919fa3ee86baa4570cc0c0ceaefd0a62f643f
nabeelrajput1 <nabeel.rajput33322@gmail.com>
2021-11-10 01:41:56 +0500

    Feedback of responsive and icons

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -10,8 +10,8 @@
     <div class="banner-text">
        <h3> I am looking for </h3>
        <h6> 
-         <a href="{{URL::to('/helpers')}}"> <img alt="helper-icon" src="{{URL::to('/')}}/assets/images/banner-icon1.jpg"> A Domestic Helper  </a>
-         <a href="{{URL::to('/agencies')}}"> <img alt="agency-icon" src="{{URL::to('/')}}/assets/images/banner-icon2.jpg">  An Agency </a>  
+         <a href="{{URL::to('/helpers')}}"> <img alt="helper-icon" src="{{URL::to('/')}}/assets/images/banner-icon1.png"> A Domestic Helper  </a>
+         <a href="{{URL::to('/agencies')}}"> <img alt="agency-icon" src="{{URL::to('/')}}/assets/images/banner-icon2.png">  An Agency </a>  
          <a href="{{URL::to('/employers')}}"> <img alt="agency-icon" src="{{URL::to('/')}}/assets/images/emp-Icon .png">  Employers </a>  
       </h6>
     </div>
